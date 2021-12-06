d8bfbb55a4917838878c089e8a04892d629c7c1d
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2021-12-06 16:48:13 +0200

    Sign Up

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <SignIn msg="Welcome to Your Vue.js App"/> -->
+    <SignUp msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import SignIn from './components/SignIn.vue'
+
+import SignUp from './components/SignUp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignUp
   }
 }
 </script>
---
+++ front-end/src/components/SignUp.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="container">
+    <h1 id="title" onclick="window.location.href='/';">MOZARK</h1>
+    <form id="login-form">
+      <input
+        type="email"
+        placeholder="UserName@seamail.com"
+        value=""
+        ref="emailRef"
+        v-model="email"
+        required
+        class="text-box"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value=""
+        ref="passwordRef"
+        v-model="password"
+        required
+        class="text-box"
+      />
+      <input
+        type="password"
+        placeholder="Repeat password"
+        value=""
+        required
+        class="text-box"
+      />
+      <input type="button" value="Sign up" id="submit" @click="SignUp()" />
+    </form>
+    <h2>
+      Already have an account?
+      <a onclick="window.location.href='/';">Sign in</a>
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#container {
+  background-color: rgb(0, 225, 255);
+  height: 100%;
+  width: 100%;
+  position: fixed;
+}
+#title {
+  min-width: 30vw;
+  border-bottom: 10px solid #ffffff;
+  border-radius: 50px;
+  padding-bottom: 15px;
+  display: inline-block;
+  font-size: 8vw;
+  color: rgb(252, 52, 52);
+  margin-left: 34%;
+  margin-top: 1%;
+  margin-bottom: 2vw;
+  font-family: "Yanone Kaffeesatz", cursive;
+  text-shadow: 0px 0px 0 rgb(236, 180, 180), 1px 0px 0 rgb(236, 180, 180),
+    2px 0px 0 rgb(202, 202, 202), 3px 0px 0 rgb(187, 187, 187),
+    4px 0px 0 rgb(173, 173, 173), 5px 0px 0 rgb(236, 180, 180),
+    6px 0px 0 rgb(144, 144, 144), 7px 0px 6px rgba(0, 0, 0, 0.6),
+    7px 0px 1px rgba(0, 0, 0, 0.5), 0px 0px 6px rgba(0, 0, 0, 0.2);
+  z-index: 11;
+  top: 0;
+  cursor: pointer;
+}
+.text-box {
+  padding: 0.5vw;
+  width: 23vw;
+  display: flex;
+  height: 3vw;
+  font-size: 2vw;
+  margin: 2vw;
+  margin-left: 36%;
+  border-radius: 30px;
+}
+#submit {
+  background-color: rgb(255, 0, 234);
+  width: 8vw;
+  height: 3vw;
+  border-radius: 20px;
+  font-size: 2vw;
+  margin-left: 44%;
+  cursor: pointer;
+}
+h2 {
+  font-size: 1.5vw;
+  margin-left: 36%;
+}
+</style>
