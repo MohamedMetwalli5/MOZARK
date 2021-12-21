cf90d34dd5c69d2a3dabba969011ddf0c8fe426b
omar rehan <omarrehan0020@gmail.com>
2021-12-21 12:50:19 +0200

    DataBase

--- backend/src/main/java/software/project/backend/BackendApplication.java
+++ backend/src/main/java/software/project/backend/BackendApplication.java
@@ -1,13 +1,25 @@
 package software.project.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import Database.User;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+	
+	 @Override
+	    public void run(String... args) throws Exception {
+	       
+	         
+	    }
 
 }
