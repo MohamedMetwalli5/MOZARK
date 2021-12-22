3eb4bb9fc2ac14a17ac1c4da1250da05c241c396
ziadalbana <58531158+ziadalbana@users.noreply.github.com>
2021-12-22 21:51:59 +0200

    package fixes

--- backend/src/main/java/software/project/backend/BackendApplication.java
+++ backend/src/main/java/software/project/backend/BackendApplication.java
@@ -1,17 +1,12 @@
 package software.project.backend;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-<<<<<<< Updated upstream
-import Database.UserDAO;
-=======
 import software.project.backend.Database.UserDAO;
->>>>>>> Stashed changes
 
 @SpringBootApplication
 public class BackendApplication implements CommandLineRunner {
@@ -26,11 +21,7 @@ public class BackendApplication implements CommandLineRunner {
 	 @Override
 	    public void run(String... args) throws Exception {
 	       UserDAO ui = new UserDAO(jdbcTemplate) ;
-<<<<<<< Updated upstream
-           Model.User uur = ui.getUserByUserName("omarrehan0020");
-=======
            software.project.backend.Model.User uur = ui.getUserByUserName("omarrehan0020");
->>>>>>> Stashed changes
            System.out.println(uur.getFirstName() + uur.getLastName());
 	         
 	    }
