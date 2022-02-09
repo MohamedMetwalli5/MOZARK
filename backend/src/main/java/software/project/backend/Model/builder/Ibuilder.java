e9fb52f3ea95cf1bb635bbdb21235c25a54f29af
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2022-02-09 17:36:46 +0200

    Delete backend directory

--- backend/src/main/java/software/project/backend/Model/builder/Ibuilder.java
+++
@@ -1,8 +0,0 @@
-package software.project.backend.Model.builder;
-
-import software.project.backend.Model.Imodel;
-
-public interface Ibuilder {
-    public boolean comopse(String sentData) ;
-    public Imodel getModel();
-}
