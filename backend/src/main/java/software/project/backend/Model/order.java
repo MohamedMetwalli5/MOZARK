fc382f86a99eed798b9b5fd327b657bea30a637c
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2022-02-09 15:36:23 +0200

    Phase 3 Done!

---
+++ backend/src/main/java/software/project/backend/Model/order.java
@@ -0,0 +1,40 @@
+package software.project.backend.Model;
+
+public class order implements Imodel{
+    private int orderID;
+    private double totalPrice;
+    private int noOfItems;
+    private String date;
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getNoOfItems() {
+        return noOfItems;
+    }
+
+    public void setNumOfItems(int numOfItems) {
+        this.noOfItems = numOfItems;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
