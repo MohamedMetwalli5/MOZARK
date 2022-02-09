10a6fa65cebcf2f0bc38fb43731e72430518b430
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2022-02-09 17:44:35 +0200

    Done!

---
+++ backend/src/main/java/software/project/backend/Model/Cart.java
@@ -0,0 +1,58 @@
+package software.project.backend.Model;
+
+public class Cart implements Imodel {
+	    private int productID;
+	    private String name;
+	    private String description;
+	    private double price;
+	    private int noOfCopies;
+	    private String image;
+
+	public int getProductID() {
+		return productID;
+	}
+
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+
+	public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public String getDescription() {
+	        return description;
+	    }
+
+	    public void setDescription(String description) {
+	        this.description = description;
+	    }
+
+	    public double getPrice() {
+	        return price;
+	    }
+
+	    public void setPrice(double price) {
+	        this.price = price;
+	    }
+
+	public int getNoOfCopies() {
+		return noOfCopies;
+	}
+
+	public void setNoOfCopies(int noOfCopies) {
+		this.noOfCopies = noOfCopies;
+	}
+
+	public String getImage() {
+	        return image;
+	    }
+
+	    public void setImage(String image) {
+	        this.image = image;
+	    }
+}
